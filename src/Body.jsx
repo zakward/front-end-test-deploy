87f0461d3702dd0aec7629f8543708389e4a38d7
Zak Ward <zakward85@gmail.com>
2024-01-31 15:00:50 -0600

    css

--- src/Body.jsx
+++ src/Body.jsx
@@ -7,17 +7,25 @@ function Body() {
                 <p>Welcome to my React App!</p>
                 <p>This is for deployment testing only!</p>
 
+                <p>**When deploying web applications, it is easiest when the project does NOT live within another repository**</p>
+                <p>**Each project to be deployed should live outside the VSchool Repository in a seperate single Repository**</p>
+
+<div id ="ul-div">
+
                 <ul>
-                    <li>Render</li>
+                    <h1>Render</h1>
                     <li>New Static Site</li>
                     <li>Connect to Repo</li>
                     <li>Build Command: $npm run build</li>
                     <li>Publish Directory: dist</li>
+                    <li>deploy</li>
                 </ul>
                 <ul>
-                    <li>Netlify</li>
+                    <h1>Netlify</h1>
                     <li>Connect to Repo</li>
+                    <li>Deploy</li>
                 </ul>
+</div>
             </main>
         </>
      );
