b8b58d2dfbf9f05655139002e9ee850bdf2d20ac
Zak Ward <zakward85@gmail.com>
2024-01-30 19:54:33 -0600

    first push

---
+++ src/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Body() {
+    return ( 
+        <>
+            <main>
+                <p>Welcome to my React App!</p>
+                <p>This is for deployment testing only!</p>
+            </main>
+        </>
+     );
+}
+
+export default Body;
