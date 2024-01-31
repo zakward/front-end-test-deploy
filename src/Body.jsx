066b3d6a06cce9347ebd67eea0d0c351a0ace946
Zak Ward <zakward85@gmail.com>
2024-01-30 20:03:55 -0600

    add

--- src/Body.jsx
+++ src/Body.jsx
@@ -6,6 +6,18 @@ function Body() {
             <main>
                 <p>Welcome to my React App!</p>
                 <p>This is for deployment testing only!</p>
+
+                <ul>
+                    <li>Render</li>
+                    <li>New Static Site</li>
+                    <li>Connect to Repo</li>
+                    <li>Build Command: $npm run build</li>
+                    <li>Publish Directory: dist</li>
+                </ul>
+                <ul>
+                    <li>Netlify</li>
+                    <li>Connect to Repo</li>
+                </ul>
             </main>
         </>
      );
